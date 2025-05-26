e848ef24e12cf3c4cab8b9047440f07751d85ec5
Kareem Bakarbashat <thought.kb@gmail.com>
2025-05-26 11:08:32 +0300

    done

---
+++ app/ui/inputs.tsx
@@ -0,0 +1,32 @@
+interface InputsProps {
+    label: string;
+    name: string;
+    placeholder: string;
+    error: string | undefined;
+    type?: string | 'text';
+    defaultValue?: string | number;
+}
+export default function Inputs({
+    label,
+    name,
+    placeholder,
+    error,
+    type,
+    defaultValue,
+}: InputsProps) {
+    return (
+        <div>
+            <label className="block text-sm font-medium text-gray-700">
+                {label}
+                <input
+                    type={type}
+                    name={name}
+                    defaultValue={defaultValue}
+                    className="mt-1 block w-full [appearance:textfield] rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none sm:text-sm [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+                    placeholder={placeholder}
+                />{' '}
+            </label>
+            {error && error.length > 0 && <p className="mt-1 ml-2 text-xs text-red-600">{error}</p>}
+        </div>
+    );
+}
