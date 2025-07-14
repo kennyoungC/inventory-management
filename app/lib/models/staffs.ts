c52fc31d52e87a78e2ddd8e88564a0eefbb07757
kennyoungC <obikenneth913@gmail.com>
2025-07-14 19:49:44 +0300

    add staff model

--- app/lib/models/staffs.ts
+++ app/lib/models/staffs.ts
@@ -7,10 +7,9 @@ export interface Staff extends Document {
     email: string;
     access_code?: number;
     job_title: string;
-    role: 'admin' | 'staff';
-    code_set: boolean;
-    verification_token?: string;
-    verification_expires?: Date;
+    code_expires_at: Date;
+    is_active: boolean;
+    last_login_at?: Date;
 }
 const StaffSchema = new Schema<Staff>(
     {
@@ -30,7 +29,7 @@ const StaffSchema = new Schema<Staff>(
             type: String,
             trim: true,
         },
-        role: { type: String, enum: ['admin', 'staff'], required: true },
+        is_active: { type: Boolean, default: false },
         email: {
             type: String,
             required: [true, 'Please provide an email'],
@@ -38,12 +37,7 @@ const StaffSchema = new Schema<Staff>(
             lowercase: true,
             validate: [validator.isEmail, 'Please provide a valid email'],
         },
-        code_set: {
-            type: Boolean,
-            default: false,
-        },
-        verification_token: { type: String, select: false },
-        verification_expires: { type: Date, select: false },
+        code_expires_at: { type: Date, required: true },
         access_code: {
             type: Number,
             required: [true, 'Access code is required'],
@@ -56,6 +50,7 @@ const StaffSchema = new Schema<Staff>(
                 message: 'Access code must be exactly 6 digits',
             },
         },
+        last_login_at: { type: Date, default: null },
     },
     {
         timestamps: true,
