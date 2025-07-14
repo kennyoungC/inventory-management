164f24b58d9a4a93e622ecdba8efd37297e310e0
kennyoungC <obikenneth913@gmail.com>
2025-07-14 19:48:21 +0300

    Add: signin server actions

--- app/lib/actions/sign-in.actions.ts
+++ app/lib/actions/sign-in.actions.ts
@@ -1,9 +1,48 @@
 'use server';
+import { signIn, signOut } from '@/auth';
+import { AuthError } from 'next-auth';
+import { object, string } from 'zod';
 
-export async function signIn(state: unknown | null, formData: FormData) {
-    const email = formData.get('email');
-    const password = formData.get('password');
+type State = { message: string } | undefined;
 
-    console.log('here the server log ', email, password);
-    return { email, password };
+const LoginSchema = object({
+    email: string({ required_error: 'Email is required' })
+        .min(1, 'Email is required')
+        .email('Invalid email'),
+    password: string({ required_error: 'Password is required' })
+        .min(1, 'Password is required')
+        .min(8, 'Password must be more than 8 characters')
+        .max(32, 'Password must be less than 32 characters'),
+});
+
+export async function logIn(prevState: State, formData: FormData): Promise<State> {
+    const email = formData.get('email')?.toString();
+    const password = formData.get('password')?.toString();
+
+    const parsed = LoginSchema.safeParse({ email, password });
+    if (!parsed.success) {
+        return { message: 'Email/password are required and must be valid.' };
+    }
+
+    try {
+        await signIn('credentials', {
+            email: parsed.data.email,
+            password: parsed.data.password,
+            redirect: true,
+            redirectTo: '/dashboard',
+        });
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return { message: 'Invalid credentials.' };
+                default:
+                    return { message: 'An unexpected error occurred.' };
+            }
+        }
+        throw error;
+    }
+}
+export async function logOut() {
+    await signOut({ redirectTo: '/sign-in' });
 }
