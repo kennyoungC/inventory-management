c6f65bb305162f351b7c106fcdd083bd0a54511d
kennyoungC <obikenneth913@gmail.com>
2025-07-21 00:44:31 +0300

    staff managemnet still porogress

--- app/dashboard/staff-management/page.tsx
+++ app/dashboard/staff-management/page.tsx
@@ -1,19 +1,14 @@
 import StaffMangement from '@/app/ui/staff-management/StaffMangement';
 import React from 'react';
 import { Metadata } from 'next';
-import { auth } from '@/auth';
-import { getAllStaff } from '@/app/lib/data';
+import { getAllStaff } from '@/app/lib/actions/staff.actions';
 
 export const metadata: Metadata = {
     title: 'Staff Management',
 };
 
 export default async function Page() {
-    const session = await auth();
-    const restaurantId = session?.user?.id;
-    if (!restaurantId) return <div>Not authorized</div>;
-
-    const staffList = await getAllStaff(restaurantId);
+    const staffList = await getAllStaff();
 
     return <StaffMangement staffList={staffList} />;
 }
