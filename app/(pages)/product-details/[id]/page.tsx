487769eb6c877d7fbdb5c97ef6b7a51d888a769a
kennyoungC <obikenneth913@gmail.com>
2025-08-14 23:24:18 +0300

    refactor(routing): remove old product-details route in favor of new dashboard path; add ProductDetails wrapper component

--- app/(pages)/product-details/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-    title: 'Product Details',
-};
-
-export default async function Page() {
-    return <div>Product Details</div>;
-}
---
+++ app/ui/product-details/ProductDetails.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderBar from '../HeaderBar';
+import { MainContent } from './MainContent';
+
+const ProductDetails = () => {
+    return (
+        <>
+            <HeaderBar title="Product Management System" />
+            <MainContent />
+        </>
+    );
+};
+
+export default ProductDetails;
