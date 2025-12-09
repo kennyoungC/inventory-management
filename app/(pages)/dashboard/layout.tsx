a7519ebfea9bfb7fcdf40d8fff613d870ba98714
kennyoungC <obikenneth913@gmail.com>
2025-12-09 18:09:04 +0200

    bug fix

--- app/(pages)/dashboard/layout.tsx
+++ app/(pages)/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden relative">
             <SideBar />
             <main
-                className="flex-grow md:overflow-y-auto transition-all duration-300 relative
+                className="grow md:overflow-y-auto transition-all duration-300 relative
                     md:ml-14 peer-hover:md:ml-64"
             >
                 {children}
